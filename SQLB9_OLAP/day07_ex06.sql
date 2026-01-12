f078b5f23685531275ca741ba921c1c88d8301d2
Kristina <82329791+Kris060991@users.noreply.github.com>
2026-01-12 20:59:23 +0300

    sqlb9

---
+++ SQLB9_OLAP/day07_ex06.sql
@@ -0,0 +1,11 @@
+SELECT 
+  pz.name, 
+  COUNT(*) AS count_of_orders,
+  round(avg(m.price), 2) AS average_price,
+  MAX(price) AS max_price,
+  MIN(price) AS min_price
+FROM menu m
+JOIN person_order po ON po.menu_id = m.id
+JOIN pizzeria pz ON pz.id = m.pizzeria_id
+GROUP BY pz.name
+ORDER BY pz.name;
